9ed8f4147f6e1e8a18531857088328e4077a18e3
Jangya <jangya069@gmail.com>
2021-06-11 20:05:25 +0530

    vhost

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5000;
+const vhost = require('vhost')
+const port = process.env.PORT || 5000;
+
 const clientDir =  path.join(__dirname, 'client');
+
+app.use(require('cookie-parser')());
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: clientDir});
@@ -10,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.get('/samesite', (req, res) => {
     const token = Math.random().toString();
-    res.cookie('sessionId', token, {
+    res.cookie('test', token, {
         sameSite: 'None',
         httpOnly: true,
         secure: true
@@ -19,9 +23,13 @@ app.get('/samesite', (req, res) => {
 });
 
 app.get('/samesite/css', (req, res) => {
-    console.log('req.cookies', req.cookies);
-    res.sendFile('samesite.css', {root: clientDir});
+    console.log('req.cookies', req.cookies['test']);
+    const cssFile = req.cookies['test'] ? 'green' : 'red';
+    res.sendFile(`${cssFile}.css`, {root: clientDir});
 });
+if(!process.env.NODE_ENV) {
+    express().use(vhost('mongohost', app));
+}
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`); 
